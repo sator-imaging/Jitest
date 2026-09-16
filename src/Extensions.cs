39bb86d67976d2fa2663f9c6b2ba8a8280092f14
sator-imaging <16752340+sator-imaging@users.noreply.github.com>
2026-09-16 06:39:17 +0000

    git restore --source=origin/main -- src/Extensions.cs

--- src/Extensions.cs
+++ src/Extensions.cs
@@ -71,48 +71,17 @@ public static class Extensions
     }
 
 
-    internal static (MethodInfo, TDelegate?) ResolveMethod<TDelegate>(Type type, string methodName)
-        where TDelegate : class
+    static (MethodInfo, TDelegate) ResolveMethod<TDelegate>(Type type, string methodName)
+        where TDelegate : Delegate
     {
-        Type[] parameters;
-        if (typeof(TDelegate) == typeof(Delegate))
-        {
-            parameters = Array.Empty<Type>();
-        }
-        else if (typeof(TDelegate).Name.StartsWith("Func", StringComparison.Ordinal))
-        {
-            parameters = typeof(TDelegate).GenericTypeArguments[..^1];
-        }
-        else if (typeof(TDelegate).Name.StartsWith("Action", StringComparison.Ordinal))
-        {
-            parameters = typeof(TDelegate).GenericTypeArguments;
-        }
-        else
-        {
-            parameters = Array.Empty<Type>();
-        }
+        var parameters = typeof(TDelegate).Name.StartsWith("Func", StringComparison.Ordinal)  // .Name == Func`N
+            ? typeof(TDelegate).GenericTypeArguments[..^1]
+            : typeof(TDelegate).GenericTypeArguments;
 
+        // Allow enum conversion
         const BindingFlags AllBindingFlags = (BindingFlags)(~0);
 
-        MethodInfo? method = null;
-        if (parameters.Length > 0 || typeof(TDelegate) != typeof(Delegate))
-        {
-            method = type.GetMethod(methodName, AllBindingFlags, binder: null, parameters, modifiers: null);
-        }
-
-        if (method == null)
-        {
-            var methods = type.GetMethods(AllBindingFlags).Where(m => m.Name == methodName).ToArray();
-            if (methods.Length == 1)
-            {
-                method = methods[0];
-            }
-            else if (methods.Length > 1 && parameters.Length > 0)
-            {
-                method = methods.FirstOrDefault(m => m.GetParameters().Length == parameters.Length);
-            }
-        }
-
+        var method = type.GetMethod(methodName, AllBindingFlags, binder: null, parameters, modifiers: null);
         if (method == null)
         {
             var bt = type.BaseType;
@@ -136,11 +105,6 @@ public static class Extensions
                 $"Method '{methodName}({string.Join(", ", parameters.Select(static x => x.Name))})' is not found in the type hierarchy of '{type}'");
         }
 
-        if (typeof(TDelegate) == typeof(Delegate))
-        {
-            return (method, null);
-        }
-
         try
         {
             var cloneMethod = OriginalMethodByMethodInfo.GetOrAdd(
@@ -150,15 +114,15 @@ public static class Extensions
                     using var dmd = new DynamicMethodDefinition(method.MethodInfo);
                     var copy = dmd.Generate();
 
-                    return copy.CreateDelegate(typeof(TDelegate));
+                    return copy.CreateDelegate<TDelegate>();
                 })
-                as TDelegate;
+                as TDelegate ?? throw new JitestException("Must not be reached");
 
             return (method, cloneMethod);
         }
-        catch (ArgumentException)
+        catch (ArgumentException ex)
         {
-            return (method, null);
+            throw new JitestException($"Method parameter type or return type doesn't match: {ex.Message}", inner: ex);
         }
     }
 }
